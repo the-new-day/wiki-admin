0ebfa8a0381088e564f8fa6f3553ddb60329ef4d
the-new-day <balakineg977@gmail.com>
2025-03-26 00:44:32 +0300

    Made --verbose and --no-log global flags

--- wiki-admin.py
+++ wiki-admin.py
@@ -4,20 +4,24 @@ from scripts.replace_text import replace_text
 from utils.logger import setup_logger
 
 def main():
-    parser = argparse.ArgumentParser(description="MediaWiki API management")
-    parser.add_argument("--verbose", action="store_true", help="Print logs")
-    parser.add_argument("--no-log", action="store_true", help="Disable logging")
+    global_parser = argparse.ArgumentParser(add_help=False)
+    global_parser.add_argument("--verbose", action="store_true", help="Print logs")
+    global_parser.add_argument("--no-log", action="store_true", help="Disable logging")
 
-    subparsers = parser.add_subparsers(dest="command")
+    parser = argparse.ArgumentParser(description="Управление MediaWiki через API", parents=[global_parser])
+    subparsers = parser.add_subparsers(dest="command", required=True)
 
-    replace_parser = subparsers.add_parser("replace", help="Mass text replace")
-    replace_parser.add_argument("--old", required=True, help="Text to replace")
-    replace_parser.add_argument("--new", required=True, help="New text")
-    replace_parser.add_argument("--pages", nargs="+", help="List of pages")
+    replace_parser = subparsers.add_parser("replace", help="Массовая замена текста", parents=[global_parser])
+    replace_parser.add_argument("--old", required=True, help="Текст для замены")
+    replace_parser.add_argument("--new", required=True, help="Новый текст")
+    replace_parser.add_argument("--pages", nargs="+", help="Список страниц")
+
+    global_args, remaining_args = global_parser.parse_known_args()
+
+    global logger
+    logger = setup_logger(verbose=global_args.verbose, disable_logging=global_args.no_log)
 
     args = parser.parse_args()
-
-    logger = setup_logger(verbose=args.verbose, disable_logging=args.no_log)
 
     if args.command == "replace":
         replace_text(args.old, args.new, args.pages)
